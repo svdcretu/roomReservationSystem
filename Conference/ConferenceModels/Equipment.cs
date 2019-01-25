51b1326dc84f7dde059102abefcec13a4c2a5415
codrutasalomia <codruta.salomia@softvision.com>
2019-01-25 19:00:23 +0200

    Added correct classes

---
+++ Conference/ConferenceModels/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceModels
+{
+	public enum Equipment
+	{
+		Webcam,
+		Jabra,
+		Phone,
+		Laptop,
+		PC,
+		Monitor,
+		Speakers
+	}
+}
---
+++ Conference/ConferenceModels/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConferenceModels
+{
+	public class User
+	{
+		public int UserId { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+	}
+}
