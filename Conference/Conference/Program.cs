bf4178c360c3b3cfbc9a4a10af56450bb5571767
codrutasalomia <codruta.salomia@softvision.com>
2019-01-30 00:20:08 +0200

    Read user input for printing

--- Conference/Conference/Program.cs
+++ Conference/Conference/Program.cs
@@ -13,50 +13,79 @@ namespace Conference
     {
         static void Main(string[] args)
         {
-            //Listing all the users
-            Console.WriteLine("Listing all the users:");
-            UserServices userServices = new UserServices();
-            List<User> userList = userServices.GetUsers();
-            foreach (User user in userList)
-            {
-                PrintUsers.PrintUserData(user);
-            }
-            Console.WriteLine(" ");
+            #region Listing
+            ////Listing all the users
+            //Console.WriteLine("Listing all the users:");
+            //UserServices userServices = new UserServices();
+            //List<User> userList = userServices.GetUsers();
+            //foreach (User user in userList)
+            //{
+            //    PrintUsers.PrintUserData(user);
+            //}
+            //Console.WriteLine(" ");
 
-            //Listing the user with a certain id
-            int userId = 3;
-            Console.WriteLine($"Listing the user with id {userId}:");
-            User selectedUser = userServices.GetUser(userId);
-            PrintUsers.PrintUserData(selectedUser);
-            Console.WriteLine(" ");
+            ////Listing the user with a certain id
+            //int userId = 3;
+            //Console.WriteLine($"Listing the user with id {userId}:");
+            //User selectedUser = userServices.GetUser(userId);
+            //PrintUsers.PrintUserData(selectedUser);
+            //Console.WriteLine(" ");
 
-            //Listing all the rooms
-            RoomServices roomServices = new RoomServices();
-            List<ConferenceRoom> roomList = roomServices.GetRooms();
-            foreach (ConferenceRoom room in roomList)
-            {
-                PrintRooms.PrintRoomDetails(room);
-            }
-            Console.WriteLine(" ");
+            ////Listing all the rooms
+            //RoomServices roomServices = new RoomServices();
+            //List<ConferenceRoom> roomList = roomServices.GetRooms();
+            //foreach (ConferenceRoom room in roomList)
+            //{
+            //    PrintRooms.PrintRoomDetails(room);
+            //}
+            //Console.WriteLine(" ");
 
-            //Listing the room with a certain id
-            int roomId = 5;
-            Console.WriteLine($"Listing the room with id {roomId}:");
-            try
+            ////Listing the room with a certain id
+            //int roomId = 1;
+            //Console.WriteLine($"Listing the room with id {roomId}:");
+            //try
+            //{
+            //    ConferenceRoom selectedRoom = roomServices.GetRoom(roomId);
+            //    PrintRooms.PrintRoomDetails(selectedRoom);
+            //}
+            //catch (InvalidOperationException ex)
+            //{
+            //    Console.WriteLine(ex.Message);                
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.WriteLine($"An unexpected exception was thrown: {e}");
+            //}
+
+            //Console.ReadLine();
+
+            #endregion
+
+            Dictionary<string, string> validOptions = new Dictionary<string, string>()
             {
-                ConferenceRoom selectedRoom = roomServices.GetRoom(roomId);
-                PrintRooms.PrintRoomDetails(selectedRoom);
-            }
-            catch (InvalidOperationException ex)
+             { "1", "Print to File"},
+             { "2", "Print to Console" }
+            };
+
+            string userInput = "";
+            bool invalidInput = true;
+            while (!validOptions.Keys.Contains(userInput))
             {
-                Console.WriteLine(ex.Message);                
+                Console.WriteLine("Please choose an option (1/2) and press Enter to apply:");
+                Console.WriteLine($"{validOptions.ElementAt(0).Key}:{validOptions.ElementAt(0).Value}");
+                Console.WriteLine($"{validOptions.ElementAt(1).Key}:{validOptions.ElementAt(1).Value}");
+
+                userInput = Console.ReadLine().Trim();
+                invalidInput = string.IsNullOrEmpty(userInput) || string.IsNullOrWhiteSpace(userInput) || !validOptions.Keys.Contains(userInput);
+
+                if (!invalidInput)
+                {
+                    var chosenOption = validOptions.SingleOrDefault(x => x.Key == userInput);
+                    Console.WriteLine($"You have chosen {chosenOption.Key}: {chosenOption.Value}");
+                }
+
             }
-            catch(Exception e)
-            {
-                Console.WriteLine($"An unexpected exception was thrown: {e}");
-            }
-            
-            
+
             Console.ReadLine();
 
         }
