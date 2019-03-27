426861cc42a99115c5e9cc4d0d909db86811b8be
codrutasalomia <codruta.salomia@softvision.com>
2019-03-27 23:43:13 +0200

    Finished homework: - Have only 2 implementation like: PrintToConsole and PrintToFile - Use these 2 implementation in the services already created

--- Conference/BusinesServices/Contracts/IPrint.cs
+++ Conference/BusinesServices/Contracts/IPrint.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using ConferenceModels;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace BusinesServices.Contracts
 {
-    public interface IPrint<T> 
+    public interface IPrint<T>
     {
         void Print(T entity);
         void Print(IList<T> entityList);
--- Conference/Conference/Program.cs
+++ Conference/Conference/Program.cs
@@ -1,8 +1,8 @@
 ﻿using BusinesServices.Contracts;
+using BusinesServices.Print;
 using BusinesServices.UserBusiness;
 using ConferenceModels;
 using ConferenceRepository.Contracts;
-using BusinesServices.Print;
 using ConferenceRepository.RoomRepository;
 using ConferenceUtils;
 using System;
@@ -14,7 +14,7 @@ namespace Conference
         static void Main(string[] args)
         {
 
-
+            #region ConferenceRoom
             RoomService roomService;
             IPrint<ConferenceRoom> roomDisplayService;
             IRepository<ConferenceRoom> roomRepository;
@@ -48,12 +48,51 @@ namespace Conference
 
             var roomList = roomService.GetData();
             roomService.Print(roomList);
-
             var room = roomService.GetDataById(2);
             roomService.Print(room);
-
             Console.ReadKey();
+            #endregion
 
+
+
+            //#region User
+            //UserService userService;
+            //IPrint<User> userDisplayService;
+            //IRepository<User> userRepository;
+
+            ////Get user repository option from user
+            //string userRepositoryOption = Utils.GetUsersRepositoryOptionFromUser();
+            //ConnectionType userConnectionType = (userRepositoryOption == "1" ? ConnectionType.File : ConnectionType.Hardcoded);
+
+            //if (userConnectionType == ConnectionType.Hardcoded)
+            //{
+            //    userRepository = new HardCodedUserRepository();
+            //}
+            //else
+            //{
+            //    userRepository = new FileBaseUserRepository();
+            //}
+
+            ////Get user print option from user
+            //string userPrintOption = Utils.GetPrintOptionFromUser();
+
+            //if (userPrintOption == "1")
+            //{
+            //    userDisplayService = new PrintToFile<User>();
+            //}
+            //else
+            //{
+            //    userDisplayService = new PrintToConsole<User>();
+            //}
+
+            //userService = new UserService(userRepository, userDisplayService);
+
+            //var userList = userService.GetData();
+            //userService.Print(userList);
+            //var user = userService.GetDataById(2);
+            //userService.Print(user);
+            //Console.ReadKey();
+            //#endregion
 
 
         }
